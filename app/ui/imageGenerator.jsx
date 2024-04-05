2ccf9b0e3e422d4859ed46932612708f0107a91b
Aurélien <2128970+GoldAndLink@users.noreply.github.com>
2024-04-05 20:43:39 +0200

    fix(imageGenerator): hide button if no assistant description

--- app/ui/imageGenerator.jsx
+++ app/ui/imageGenerator.jsx
@@ -1,5 +1,5 @@
-'use client'
-import {useState} from "react";
+"use client";
+import { useState } from "react";
 
 export default function ImageGenerator({ paintingDescription }) {
   const [image, setImage] = useState(null);
@@ -7,36 +7,40 @@ export default function ImageGenerator({ paintingDescription }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('/api/generate-image', {
-            method: 'POST',
-            body: JSON.stringify({
-                paintingDescription
-            }),
-        });
-        setImage(response?.url);
-        // const data = await response.json();
-        // console.log('Generated image:', data);
+      const response = await fetch("/api/generate-image", {
+        method: "POST",
+        body: JSON.stringify({
+          paintingDescription,
+        }),
+      });
+      setImage(response?.url);
+      // const data = await response.json();
+      // console.log('Generated image:', data);
     } catch (error) {
-        console.error('Error generating image:', error);
-        // Todo: Show an error message to the user
+      console.error("Error generating image:", error);
+      // Todo: Show an error message to the user
     }
-        
   };
   // Todo: Add a loader
-    return (
-        <div>
-            {image ? (
-              <div className="mt-8 rounded-lg shadow-lg overflow-hidden">
-                <img src={image} alt="Generated Art" className="w-full h-auto" />
-              </div>
-            ) : (
-              <button 
-                onClick={handleSubmit}
-                className="mt-8 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-              >
-                Generate this art piece
-              </button>
-            )}
+
+  if (!paintingDescription) {
+    return null;
+  }
+
+  return (
+    <div>
+      {image ? (
+        <div className="mt-8 rounded-lg shadow-lg overflow-hidden">
+          <img src={image} alt="Generated Art" className="w-full h-auto" />
         </div>
-    )
+      ) : (
+        <button
+          onClick={handleSubmit}
+          className="mt-8 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Generate this art piece
+        </button>
+      )}
+    </div>
+  );
 }
